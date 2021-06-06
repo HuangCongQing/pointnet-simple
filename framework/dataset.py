ea43386be1bb331ca8c0ed3403b92dbc5c19361e
HuangCongQing <1756260160@qq.com>
2021-06-06 21:06:58 +0800

    add

--- framework/dataset.py
+++ framework/dataset.py
@@ -4,7 +4,7 @@ Author: HCQ
 Company(School): UCAS
 Email: 1756260160@qq.com
 Date: 2021-06-05 11:19:36
-LastEditTime: 2021-06-06 21:02:31
+LastEditTime: 2021-06-06 21:06:53
 FilePath: /pointnet-simple/framework/dataset.py
 '''
 import torch
@@ -111,7 +111,7 @@ if __name__ == "__main__":
   train_data = PointNetDataset("/home/hcq/data/modelnet40/modelnet40_normal_resampled", train=0) #txt文件   9843
   train_loader = DataLoader(train_data, batch_size=2, shuffle=True) # 官方DataLoader  4922
   cnt = 0
-  print(" len(train_loader:", len(train_loader))
+  print(" len(train_loader:", len(train_loader))# 4922
   for pts, label in train_loader: #  batch_size=2  循环2次
     print("pts.shape", pts.shape) # torch.Size([2, 3, 10000])
     print("label.shape", label.shape) # torch.Size([2, 40])
